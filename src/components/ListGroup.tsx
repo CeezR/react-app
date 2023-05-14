dc5ddd1d519720ff248944390836c936647b3782
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-14 11:52:45 +0200

    feat: Implemented Hook for selectedIndex

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,21 @@
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //items = [];
-  let selectedIndex = 0;
+  // items = [];
+  // let selectedIndex = 0; // This variable is local to this function component, react is not aware of it!
+  // To solve this people we need to tell React that his component is going to have data or state that
+  // is going to change over time which is done using hooks.
+
+  // Hook
+  // useState() returns an array with a variable & updater function
+  // useSate(-1) returns an array with a var initialize with the value of -1 $ updater
+  const arr = useState(-1);
+  arr[0]; // variable (selectedIndex)
+  arr[1]; // updater function
+  // Using the updater function we can update the variable, which in addition to updating
+  // the variable notifies React of the state change. React knowing the state has changed
+  // will intern rerender the componentes equating to the DOM being updated.
 
   // Use variable when you don't need to specify parameters
   const message = items.length === 0 ? <p>No item found</p> : null;
@@ -25,12 +37,10 @@ function ListGroup() {
         {items.map((item, index) => (
           <li
             className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
+              arr[0] === index ? "list-group-item active" : "list-group-item"
             }
             key={item}
-            onClick={() => (selectedIndex = index)}
+            onClick={() => arr[1](index)}
           >
             {item}
           </li>
