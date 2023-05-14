fe0e76523fd1721c52f2216cf237da1772d38590
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-14 10:30:30 +0200

    feat: Implemented if condition to handle empty responce

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  items.map((item) => <li className="list-group-item">{item}</li>);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
   // Empty angle brackets tells react to use a fragment to all its children!
+  // React requires that all child items have a unique key, in this case
+  // the strings are all unique however normaly you would have a dedicated
+  // id field for the item object you are creating.
   return (
     <>
       <h1>List Group</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
