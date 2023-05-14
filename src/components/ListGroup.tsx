135e87f9f639b4bdf996c0717ba609a9558a84d2
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-14 10:22:02 +0200

    Chore: React require unique key for each child in list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,7 +9,9 @@ function ListGroup() {
       <h1>List Group</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item">{item}</li>
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
         ))}
       </ul>
     </>
