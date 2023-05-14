a41363e286e1a126bb3a01cd6dd6ea6146422111
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-14 12:34:29 +0200

    refactor: destructured state variable

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -10,12 +10,28 @@ function ListGroup() {
   // Hook
   // useState() returns an array with a variable & updater function
   // useSate(-1) returns an array with a var initialize with the value of -1 $ updater
-  const arr = useState(-1);
-  arr[0]; // variable (selectedIndex)
-  arr[1]; // updater function
+  // const arr = useState(-1);
+  // arr[0]; // variable (selectedIndex)
+  // arr[1]; // updater function
   // Using the updater function we can update the variable, which in addition to updating
   // the variable notifies React of the state change. React knowing the state has changed
   // will intern rerender the componentes equating to the DOM being updated.
+
+  // The prefured method for implementing hook is to desctructure the array like below!
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // Due to the fact that we destructure the array
+  /*
+  To destructure an array means to extract values from an array and assign them to individual 
+  variables in a single step. This feature is commonly found in programming languages that 
+  support pattern matching or destructuring assignment.
+  Destructuring an array allows you to access its elements directly and use them in 
+  your code without explicitly accessing each element by its index. 
+  It provides a convenient and concise way to extract values from an array and assign 
+  them to variables. 
+
+  Thanks to this we can update the variable with this: setSelectedIndex(1) rather then this: arr[1](1)
+  */
 
   // Use variable when you don't need to specify parameters
   const message = items.length === 0 ? <p>No item found</p> : null;
@@ -37,10 +53,12 @@ function ListGroup() {
         {items.map((item, index) => (
           <li
             className={
-              arr[0] === index ? "list-group-item active" : "list-group-item"
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
             }
             key={item}
-            onClick={() => arr[1](index)}
+            onClick={() => setSelectedIndex(index)}
           >
             {item}
           </li>
